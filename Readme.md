ab1076108a07ed30155119f2ffc84449d1012f1d
Stéphane Dionisio <stephane.dionisio1@gmail.com>
2020-08-22 15:26:04 +0200

    Add an android support for Sender class.

--- Readme.md
+++ Readme.md
@@ -38,7 +38,7 @@ include ':[MyProject]', ':libs:simple_network_lib'
 project(':libs:simple_network_lib').projectDir = new File(settingsDir, 'libs/simple_network_lib')
 ```
 ### Android project
-in **/build.gradle**
+in **app/build.gradle**
 ```gradle
 dependencies {
     ...
@@ -47,17 +47,17 @@ dependencies {
 
 task getLibs {
     doFirst {
-        if(!file('app/libs/').exists())
-            mkdir 'app/libs'
-        if(!file('app/libs/simple_network_lib/').exists())
-            mkdir 'app/libs/simple_network_lib'
-        if(!file('app/libs/simple_network_lib/.git').exists())
+        if(!file('libs/').exists())
+            mkdir 'libs'
+        if(!file('libs/simple_network_lib/').exists())
+            mkdir 'libs/simple_network_lib'
+        if(!file('libs/simple_network_lib/.git').exists())
             exec {
-                commandLine 'git', 'clone', 'git@github.com:StephaneDionisio/simple_network_lib.git', 'app/libs/simple_network_lib'
+                commandLine 'git', 'clone', 'git@github.com:StephaneDionisio/simple_network_lib.git', 'libs/simple_network_lib'
             }
         else
             exec {
-                workingDir 'app/libs/simple_network_lib'
+                workingDir 'libs/simple_network_lib'
                 commandLine 'git', 'pull'
             }
     }
---
+++ src/main/java/snetwork/android/sender/AndroidAbstractP2PSender.java
@@ -0,0 +1,75 @@
+package snetwork.android.sender;
+
+import android.app.Application;
+import android.content.Context;
+import android.net.wifi.WifiManager;
+import snetwork.sender.AbstractP2PSender;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.util.ArrayList;
+
+/**
+ * Class that can be used to make an Android device as a sender. It will
+ * search peers in the local network (wifi) or in the list of the connected devices (hotspot).
+ */
+public abstract class AndroidAbstractP2PSender extends AbstractP2PSender {
+
+    /**
+     * The owner app.
+     */
+    private Application owner;
+
+    /**
+     * <i><b>AbstractP2PSender</b></i>
+     *
+     * <pre> protected AbstractP2PSender(int port) </pre>
+     *
+     * Constructor of {@link AbstractP2PSender}.
+     * @param port    the port used.
+     * @param timeout the socket timeout in milliseconds for the search of peers.
+     */
+    protected AndroidAbstractP2PSender(int port, int timeout, Application owner) {
+        super(port, timeout);
+        this.owner = owner;
+    }
+
+    @Override
+    protected boolean searchPeer() {
+        WifiManager wifi = (WifiManager) owner
+                .getBaseContext()
+                .getApplicationContext()
+                .getSystemService(Context.WIFI_SERVICE);
+
+        boolean isHotspot = wifi == null || !wifi.isWifiEnabled();
+        if(wifi == null)
+            throw new RuntimeException("getSystemService(Context.WIFI_SERVICE) returns null.");
+
+        // Broadcast
+        if(!isHotspot)
+            return super.searchPeer();
+
+        // else /proc/net/arp -> list on connected devices
+
+        ArrayList<InetAddress> addresses = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    new FileInputStream(new File("/proc/net/arp"))));
+
+            String line;
+            // skip line 1
+            reader.readLine();
+
+            while ((line = reader.readLine()) != null)
+                addresses.add(InetAddress.getByName(line.split(" ")[0]));
+
+        } catch (IOException e) {
+            // FileNotFoundException => Not supported by this phone.
+            e.printStackTrace();
+            return false;
+        }
+
+        return searchPeer(addresses);
+    }
+
+}
